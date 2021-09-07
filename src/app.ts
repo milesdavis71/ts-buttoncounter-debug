005bfdbe7795707717387a5d59ef357a91eac97b
milesdavis71 <hevesiistvan71@hotmail.com>
2021-09-07 19:24:02 +0200

    counter kész

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,31 @@
 
 window.addEventListener('load', function () {
+    const counterContainer = document.querySelector('.count')
+    
     let counter = 0
     const btn = document.querySelector('button');
     btn?.addEventListener('click', function () {
-        counter++;
-        console.log(counter)
+        const text = document.querySelectorAll('p')
+        for (let i = 0; i < text.length; i++) {
+            text[i].innerHTML = '';
+        }
+   counter++;
+            if(counterContainer){
+                counterContainer.insertAdjacentHTML("beforeend", `<p>${counter}</p>`)
+                
+            }
+           
+
+  
+        
+
+
     })
-
-
     
+   
 })
+
+
 
 // btn?.addEventListener('click', function (event) {
 //   console.log(event.type);
