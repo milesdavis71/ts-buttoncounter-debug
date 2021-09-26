b2ef81c9bba9f2ee45ccc202e5bb5b6898fa7ed1
Hevesi Istvan <hevesiistvan71@hotmail.com>
2021-09-26 07:22:05 +0200

    Delete dist directory

--- dist/model.js
+++
@@ -1,9 +0,0 @@
-export const state = {
-    // Ez jelenik meg az oldalon a view-ból történő első markup betöltéskor.
-    counter: 0,
-};
-// Felülírja a state-et a gombnyomás hatására
-export const updateCounter = function (newCounter) {
-    state.counter = newCounter;
-};
-//# sourceMappingURL=model.js.map
