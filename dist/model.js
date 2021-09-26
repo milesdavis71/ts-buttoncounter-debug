925390489816f83e5bf7b53d0fa73936f49f1869
milesdavis71 <hevesiistvan71@hotmail.com>
2021-09-26 07:18:28 +0200

    dist kizárása folyamatban.

--- dist/model.js
+++ dist/model.js
@@ -1,9 +1,2 @@
-export const state = {
-    // Ez jelenik meg az oldalon a view-ból történő első markup betöltéskor.
-    counter: 0,
-};
-// Felülírja a state-et a gombnyomás hatására
-export const updateCounter = function (newCounter) {
-    state.counter = newCounter;
-};
+"use strict";
 //# sourceMappingURL=model.js.map
